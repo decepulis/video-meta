4898e86e20e5fdabda07358748dc69d6cdd32b21
Darius Cepulis <dcepulis@mux.com>
2024-03-29 08:50:01 -0500

    we should use params instead of queryparams; cache

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel({
+    // by default, this caches for the duration of the deployment.
+    // in production, you'd want to use mux webhooks to notify your deployment
+    // that something's changed and it should invalidate the cache.
+    isr: true,
+  }),
 });
